48a8d5d24a557c0db69259c9b6ac76336e868b10
Baldanz00 <gioia.travaglini18@gmail.com>
2025-05-14 20:11:53 +0200

    02-

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,11 +10,13 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
-
+const reversedTeachers = teachers.slice().reverse();
+console.log(reversedTeachers)
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.filter(name => name.length >= 5); 
+//dalla documentazione online ho trovato .filter che dovrebbe selezionare solo alcuni nomi
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
